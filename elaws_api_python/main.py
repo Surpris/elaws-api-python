20bd80d3ca14706b7649d843622b2f6b4efee8c7
Toshiyuki Nishiyama Hiraki <take90-it09-easy27@outlook.jp>
2023-07-15 06:44:18 +0000

    udpate main.py

--- elaws_api_python/main.py
+++ elaws_api_python/main.py
@@ -1,60 +1,33 @@
 """elaws_api_python.main
 """
 
-import requests
-from typing import List, Dict, Optional
-
-
-TIMEOUT_SEC = 30.0
+from .base import (
+    request_laws_and_ordinances,
+    TIMEOUT_SEC
+)
+from .classes import ListOfLaws
 
 
 def acquire_laws_and_ordinances(
     version: int, lawtype: int,
     timeout: float = TIMEOUT_SEC
-) -> Optional[List[Dict[str, str]]]:
+) -> ListOfLaws:
     """
     Acquire a list of laws and ordinances.
 
-    Args:
-        version (int): Version number of the e-Gov eLaw API.
-        lawtype (int): Law type.
+    Parameters
+    ----------
+    version : int
+        Version number of the e-Gov eLaw API.
+    lawtype : int
+        Law type.
+    timeout : float, optional
+        Timeout duration in seconds. Default is TIMEOUT_SEC.
 
-    Returns:
-        Optional[List[Dict[str, str]]]: A list of dictionaries containing the acquired laws and ordinances.
-            Each dictionary has the following keys:
-            - 'law_id' (str): Law ID.
-            - 'law_name' (str): Law name.
-            - 'law_no' (str): Law number.
-            - 'promulgation_date' (str): Promulgation date.
-
-        Returns None if an error occurs during the API request.
-
-    Raises:
-        requests.exceptions.RequestException: If an error occurs during the API request.
-
-    Example:
-        laws = acquire_laws_and_ordinances(version=1, lawtype=2)
-        if laws:
-            for law in laws:
-                print(f"Law ID: {law['law_id']}")
-                print(f"Law Name: {law['law_name']}")
-                print(f"Law Number: {law['law_no']}")
-                print(f"Promulgation Date: {law['promulgation_date']}")
-                print()
+    Returns
+    -------
+    ListOfLaws
+        The list of laws and ordinances.
     """
-    url = f"https://elaws.e-gov.go.jp/api/{version}/lawlists/{lawtype}"
-
-    try:
-        response = requests.get(url, timeout=timeout)
-        response.raise_for_status()  # Raise exception for non-200 status codes
-        xml_data = response.content
-
-        # Process the XML data
-        # You can use any XML parsing library like lxml, xml.etree.ElementTree, etc.
-
-        # TODO: Parse the XML data and extract the required information
-
-        return response.content  # Replace with the extracted data
-
-    except requests.exceptions.RequestException as e:
-        raise e
+    content = request_laws_and_ordinances(version, lawtype, timeout)
+    return ListOfLaws(content)
